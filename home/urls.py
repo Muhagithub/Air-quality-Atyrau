16252ca816b7da3878f327d754daa7e5c99589d0
Muhagithub <146169316+Muhagithub@users.noreply.github.com>
2024-02-12 21:12:35 +0500

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,4 +1,12 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.index), path("weather-atyrau", views.weather)]
+urlpatterns = [
+    path("", views.index),
+    path("weather-atyrau", views.weather),
+    path("index-russian", views.russian),
+    path("index-english", views.english),
+    path("weather-russian", views.weather_russian),
+    path("weather-english", views.weather_english),
+]
+
