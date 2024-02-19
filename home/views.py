fa6f50afb10a1dd19926a637b5ff81b33430e675
Muhagithub <146169316+Muhagithub@users.noreply.github.com>
2024-02-19 09:59:42 +0500

    Update views.py

--- home/views.py
+++ home/views.py
@@ -22,7 +22,7 @@ def index(request):
     if minute <= 9:
         minute = "0" + str(minute)
 
-    url_aqi = "https://api.waqi.info/feed/A217510/?token=6ed024b9165f16b154982ceca71f3c108a7e6047"
+    url_aqi = "https://api.waqi.info/feed/A401917/?token=6ed024b9165f16b154982ceca71f3c108a7e6047"
 
     responce = requests.get(url_aqi)
 
@@ -341,7 +341,7 @@ def russian(request):
     if minute <= 9:
         minute = "0" + str(minute)
 
-    url_aqi = "https://api.waqi.info/feed/A217510/?token=6ed024b9165f16b154982ceca71f3c108a7e6047"
+    url_aqi = "https://api.waqi.info/feed/A401917/?token=6ed024b9165f16b154982ceca71f3c108a7e6047"
 
     responce = requests.get(url_aqi)
 
@@ -380,7 +380,7 @@ def english(request):
     if minute <= 9:
         minute = "0" + str(minute)
 
-    url_aqi = "https://api.waqi.info/feed/A217510/?token=6ed024b9165f16b154982ceca71f3c108a7e6047"
+    url_aqi = "https://api.waqi.info/feed/A401917/?token=6ed024b9165f16b154982ceca71f3c108a7e6047"
 
     responce = requests.get(url_aqi)
 
